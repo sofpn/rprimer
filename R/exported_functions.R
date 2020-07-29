d4417bacfe09453ea075e0461f2f2798ff1e1a3a
sofpn <sofia.persson@slv.se>
2020-07-29 14:17:43 +0200

    edited add_probes
    Former-commit-id: 03c29ddd48b40dcdeaa40449a2a6b2ef873f9a7b
    Former-commit-id: 5bdd54c31fe893cfd61ef351caa2d84d2be9ae14

--- R/exported_functions.R
+++ R/exported_functions.R
@@ -498,8 +498,7 @@ get_oligos <- function(
 #' assays. An error message will return if no assays are found.
 #'
 #' @examples
-#' get_assays(example_rprimer_oligo, length = 60:150, max_tm_difference = 1)
-#'
+#' get_assays(example_rprimer_oligo, length = 60:150, max_tm_difference = 4)
 get_assays <- function(x, length = 65:120, max_tm_difference = 1) {
     if (!inherits(x, "rprimer_oligo")) {
         stop("An rprimer_oligo object is expected.", call. = FALSE)
@@ -580,7 +579,6 @@ get_assays <- function(x, length = 65:120, max_tm_difference = 1) {
 #' means that the Tm of the probe is lower than the average Tm of the
 #' primer pair.
 #'
-#' #Warning
 #' The Tm-difference is calculated from the majority oligos, and
 #' may thus be misleading for degenerate (iupac) oligos.
 #'
@@ -600,47 +598,65 @@ add_probes <- function(x, y, tm_difference = c(0, 20)) {
     if (!inherits(y, "rprimer_oligo")) {
         stop("An rprimer_oligo object is expected for y.", call. = FALSE)
     }
-    if (!(min(tm_difference) >= -20 && max(tm_difference) <= 20 && is.numeric(tm_difference))) {
-        stop("tm_difference must be between -30 and 30, e.g. c(-1, 5)", call. = FALSE)
+    if (!(min(tm_difference) >= -20 && max(tm_difference) <= 20)) {
+        stop(
+          "tm_difference must be between -30 and 30, e.g. c(-1, 5)",
+          call. = FALSE
+        )
     }
     assays <- x
     probes <- y
     # For each assay, take all probes that bind whithin the assay region
     probe_candidates <- purrr::map(seq_len(nrow(assays)), function(i) {
-        # The probe has to begin after the fwd primer ends and we want at least one base inbetween (hence the + 1)
+        # The probe has to begin after the fwd primer ends
+        # and we want at least one base inbetween (hence the + 1)
         from <- assays$end_fwd[[i]] + 1
-        # The probe has to end before the rev primer begins and we want at least one base inbetween
+        # The probe has to end before the rev primer begins
+        # and we want at least one base inbetween
         to <- assays$begin_rev[[i]] - 1
         # Take all probes that begin and end 'within' the assay region
-        probe <- probes[which(probes$begin >= from & probes$end <= to), ]
+        probe <- probes[probes$begin >= from & probes$end <= to, ]
         probe
     })
     # For each assay, we check how many probe candidates we have
     number_of_probes <- purrr::map_int(probe_candidates, nrow)
-    # Then, we need to pick all the assays that can harbour a probe.  For assays that has n number of probes, we need to
+    # Then, we need to pick all the assays that can harbour a probe.
+    # For assays that has n number of probes, we need to
     # repeat that row n times.
-    rows_to_select <- purrr::map(seq_along(number_of_probes), ~rep(.x, number_of_probes[[.x]]))
+    rows_to_select <- purrr::map(
+      seq_along(number_of_probes), ~rep(.x, number_of_probes[[.x]])
+    )
     rows_to_select <- unlist(rows_to_select, use.names = FALSE)
     assays <- assays[rows_to_select, ]
     if (nrow(assays) == 0L)
         stop("No assays could be generated.", call. = FALSE)
     # Now, we can make a data frame of the probe candidates
     probe_candidates <- do.call("rbind", probe_candidates)
-    # Check the sense of the probe, if the plus sense probe is ok, (i.e. not NA), we take that one.
+    # Check the sense of the probe
+    # if the plus sense probe is ok, (i.e. not NA), we take that one.
     sense <- ifelse(!is.na(probe_candidates$majority), "pos", "neg")
-    probe_candidates$majority <- ifelse(sense == "pos", probe_candidates$majority, probe_candidates$majority_rc)
-    probe_candidates$iupac <- ifelse(sense == "pos", probe_candidates$iupac, probe_candidates$iupac_rc)
+    probe_candidates$majority <- ifelse(
+      sense == "pos", probe_candidates$majority, probe_candidates$majority_rc
+    )
+    probe_candidates$iupac <- ifelse(
+      sense == "pos", probe_candidates$iupac, probe_candidates$iupac_rc
+    )
     probe_candidates <- tibble::add_column(probe_candidates, sense)
     drop <- c("majority_rc", "iupac_rc")
-    probe_candidates <- probe_candidates[, -which(names(probe_candidates) %in% drop)]
+    probe_candidates <- probe_candidates[, !(names(probe_candidates) %in% drop)] ### Does this work?
     names(probe_candidates) <- paste0(names(probe_candidates), "_pr")
     assays <- dplyr::bind_cols(assays, probe_candidates)
     assays$total_degeneracy <- assays$total_degeneracy + probe_candidates$degeneracy_pr
-    tm_difference_primer_probe <- purrr::map_dbl(seq_len(nrow(assays)), ~assays$tm_majority_pr[[.x]] - mean(assays$tm_majority_fwd[[.x]],
-        assays$tm_majority_rev[[.x]]))
-    assays <- tibble::add_column(assays, tm_difference_primer_probe, .after = "tm_difference_primer")
-    assays <- assays[which(assays$tm_difference_primer_probe >= min(tm_difference) & assays$tm_difference_primer_probe <=
-        max(tm_difference)), ]
+    tm_difference_primer_probe <- purrr::map_dbl(seq_len(nrow(assays)), function(x) {
+      assays$tm_majority_pr[[x]] - mean(
+        assays$tm_majority_fwd[[x]], assays$tm_majority_rev[[x]]
+      )
+    })
+    assays <- tibble::add_column(
+      assays, tm_difference_primer_probe, .after = "tm_difference_primer"
+    )
+    assays <- assays[assays$tm_difference_primer_probe >= min(tm_difference), ]
+    assays <- assays[assays$tm_difference_primer_probe <= max(tm_difference), ]
     if (nrow(assays) == 0L)
         stop("No assays could be generated.", call. = FALSE)
     assays <- tibble::new_tibble(assays, class = "rprimer_assay")
