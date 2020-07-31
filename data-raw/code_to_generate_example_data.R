bf6bfe8ebbbd368fd8bf30a95327b9d895392174
sofpn <sofia.persson@slv.se>
2020-07-31 22:44:00 +0200

    updated code to generate example data
    Former-commit-id: f93582d941718d17af0d60e46508ea757449488f
    Former-commit-id: 29bf394d1c66422ebb5e25f44e0b2f9a8682fca6

--- data-raw/code_to_generate_example_data.R
+++ data-raw/code_to_generate_example_data.R
@@ -1,31 +1,31 @@
 # Code to generate example data
 
-infile <- "example_alignment.txt"
+# infile <- "example_alignment.txt"
 
-example_rprimer_alignment <- read_fasta_alignment(infile) %>%
-  remove_gaps(., threshold = 0.5)
+# example_rprimer_alignment <- read_fasta_alignment(infile) %>%
+#  remove_gaps(., threshold = 0.5)
 
-example_rprimer_sequence_profile <- sequence_profile(example_rprimer_alignment)
+# example_rprimer_sequence_profile <- sequence_profile(example_rprimer_alignment)
 
-example_rprimer_sequence_properties <- sequence_properties(
-  example_rprimer_sequence_profile, iupac_threshold = 0.1
-)
+# example_rprimer_sequence_properties <- sequence_properties(
+#  example_rprimer_sequence_profile, iupac_threshold = 0.1
+# )
 
-example_rprimer_oligo <- get_oligos(
-  example_rprimer_sequence_properties,
-  example_rprimer_alignment,
-  max_gap_frequency = 0.1,
-  length = 18:22,
-  max_degenerates = 2,
-  max_degeneracy = 4,
-  gc_range = c(0.45, 0.56),
-  tm_range = c(48, 65),
-  avoid_3end_ta = TRUE
-)
+# example_rprimer_oligo <- get_oligos(
+#  example_rprimer_sequence_properties,
+#  example_rprimer_alignment,
+#  max_gap_frequency = 0.1,
+#  length = 18:22,
+#  max_degenerates = 2,
+#  max_degeneracy = 4,
+#  gc_range = c(0.45, 0.56),
+#  tm_range = c(48, 65),
+#  avoid_3end_ta = TRUE
+#)
 
-example_rprimer_assay <- get_assays(
-  example_rprimer_oligo, length = 60:150, max_tm_difference = 1.5
-)
+#example_rprimer_assay <- get_assays(
+#  example_rprimer_oligo, length = 60:150, max_tm_difference = 1.5
+#)
 
 #save(example_rprimer_alignment, file = "example_rprimer_alignment.RData")
 #save(example_rprimer_sequence_profile, file = "example_rprimer_sequence_profile.RData")
