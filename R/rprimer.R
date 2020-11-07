2f045a146e2be19c4b9f75b42b29803f1af66125
sofpn <sofia-persson@slv.se>
2020-11-07 21:06:19 +0100

    vingette
    Former-commit-id: 40561d1d018cd250268ae6e987ad89a6db5d0c1f
    Former-commit-id: 1c2cff38b423a2194098da72b3d89d0a70999480

---
+++ R/rprimer.R
@@ -0,0 +1,7 @@
+#' rprimer package
+#'
+#' rprimer provides tools for designing broadly reactive (RT)-(q/dd)PCR assays
+#' from multiple DNA sequence alignments.
+#' The design process is built on three functions:
+#'
+#' For more information, please see the package vingette.
