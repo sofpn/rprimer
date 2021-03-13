5a0122030a069e5bd3ec25c1ba3e9ccff7cb258f
sofpn <sofia.persson@slv.se>
2021-03-13 17:33:02 +0100

    added a test

---
+++ tests/testthat/test-workflow.R
@@ -0,0 +1,7 @@
+test_that("Data import and workflow works." {
+    infile <- system.file("extdata", "example_alignment.txt", package = "rprimer")
+    aln <- Biostrings::readDNAMultipleAlignment(infile, format = "fasta")
+    consensus <- consensusProfile(aln, 0.1)
+    ols <- oligos(consensus[1:2000, ])
+    expect_s4_class(ols, "RprimerOligo")
+})
