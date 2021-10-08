1fb56978f5331ae4a9c590d4e077220221135c2c
sofpn <sofia.persson@slv.se>
2021-10-08 07:03:13 +0200

    changed a test

--- tests/testthat/test-consensusProfile.R
+++ tests/testthat/test-consensusProfile.R
@@ -23,7 +23,7 @@ test_that("consensusProfile works with a rowmask", {
     expect_true(all(prof$identity == 1))
     expect_true(all(prof$entropy == 0))
     expect_true(all(prof$gaps == 0 | prof$gaps == 1))
-    expect_true(all(prof$coverage == 1))
+    expect_true(all(prof$coverage == 1 | prof$coverage == 0))
 })
 
 test_that("consensusProfile works with a colmask", {
